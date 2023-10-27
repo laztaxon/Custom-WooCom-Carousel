d04bc2ef91701a1232bdbecba88adac30a3611cc
Cello Rondon <cello@Cellos-MacBook-Pro.local>
2023-10-27 13:57:42 -0400

    fixing styling issues on dots and hide for web

--- my-carousel-script.js
+++ my-carousel-script.js
@@ -1,28 +1,39 @@
-// kick off the polyfill!
 window.__forceSmoothScrollPolyfill__ = true;
 
 jQuery(document).ready(function($) {
+    var $carousel = $('.my-carousel'); // Define $carousel variable
+
     // Initialize the carousel
-    $('.my-carousel').slick({
+    $carousel.slick({
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         autoplay: false,
         speed: 500,
         cssEase: 'linear',
-        dots: true, // Add this line
+        dots: true, //Dot Navigation
+        arrows: false, // remove arrow nav
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 960,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 670,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
     });
+
+    // Hide dots if there's only one page
+    var slideCount = $carousel.slick('getSlick').slideCount;
+    var slidesToShow = $carousel.slick('getSlick').options.slidesToShow;
+    if (slideCount <= slidesToShow) {
+        $carousel.find('.slick-dots').hide();
+    }
 });
